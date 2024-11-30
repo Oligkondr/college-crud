7d2f505d1d7cb4ab81e7c96a7eddc60440bfdec3
olegk <oligkondr@mail.ru>
2024-11-30 16:34:33 +0300

    Made header

--- src/components/TopSearch.vue
+++ src/components/TopSearch.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  Search
+  <section class="bg-white p-[20px] drop-shadow-xl flex items-center">
+    <div class="min-h-[50px] min-w-[50px] rounded-full bg-[#9873ff] flex items-center justify-center">
+      <p class="text-white font-bold">skb.</p>
+    </div>
+    <input type="text" class="w-[581px] ml-[53px] px-4 py-3 border" placeholder="Введите запрос">
+  </section>
 </template>
 
 <style scoped>
