551718f23825bb53303274e11885a6e87d51eaae
olegk <oligkondr@mail.ru>
2024-12-08 13:02:40 +0300

    Modal forms in progress

--- src/components/AddContacts.vue
+++ src/components/AddContacts.vue
@@ -1,31 +1,27 @@
 <script setup lang="ts">
 import ContactFormRow from '@/components/ContactFormRow.vue'
 import IconCrossPurple from '@/components/icons/IconCrossPurple.vue'
-import {useClientsFormStore} from '@/stores/clientsForm.ts'
+import { useClientsFormStore } from '@/stores/clientsForm.ts'
 
 const store = useClientsFormStore()
 const contacts = store.state.contacts
-
-let id = 0
 
 const addContact = () => {
   contacts.push({
     'type': 'Телефон',
     'value': ''
   })
-  id++
   console.log(contacts)
-  console.log(id)
 }
 </script>
 
 <template>
   <div class="mt-6 px-7 py-6 bg-[#C8C5D1] bg-opacity-20 flex flex-col">
     <div class="flex flex-col gap-3.5">
-      <ContactFormRow v-for="contact in contacts" :id="id"/>
+      <ContactFormRow v-for="(contact, index) in contacts" :id="index" />
     </div>
     <button class="mt-6 flex items-center justify-center" @click="addContact">
-      <IconCrossPurple/>
+      <IconCrossPurple />
       <span class="ml-1">
         Добавить контакт
       </span>
--- src/components/ContactFormRow.vue
+++ src/components/ContactFormRow.vue
@@ -9,15 +9,19 @@ const props = defineProps({
   id: Number
 })
 
+const id = props.id
+
 const deleteRow = () => {
-  // const newContacts = contacts.filter((contact, id) => id != props.id)
-  console.log(props.id)
+  // contacts.splice(props.id,1)
+  console.log(id)
+  console.log(contacts)
 }
 </script>
 
 <template>
+  {{contacts}}
   <div class="flex">
-    <select class="h-9 w-1/3 pl-3 bg-[#E7E5EB] border border-[#C8C5D1] text-xs">
+    <select class="h-9 w-1/3 pl-3 bg-[#E7E5EB] border border-[#C8C5D1] text-xs" v-model="contacts[id]">
       <option value="phone">Телефон</option>
       <option value="email">Email</option>
       <option value="facebook">Facebook</option>
